8c76282431b0442fae956ac65294a1f2384bf36e
nishu321 <nissarg37@gmail.com>
2021-03-08 17:33:50 -0500

    Finished refresher on variables,string formatting,and user input

---
+++ PythonRefresher/python_refresher.py
@@ -0,0 +1,15 @@
+# Section 2
+
+# variables (already understand this section)
+
+# string formatting
+# f string
+name = "Joe"
+greeting = f"Hello, {name}"
+print(greeting)
+
+x = 1000
+
+print(f"{x:,}")
+# get input (already understand this section)
+
