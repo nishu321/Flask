d1e4ba9f975feb6903125eccb21d7a5a469a639f
nishu321 <nissarg37@gmail.com>
2021-03-09 13:39:49 -0500

    Finished refresher section 2 part 17-20

--- PythonRefresher/python_refresher.py
+++ PythonRefresher/python_refresher.py
@@ -47,7 +47,7 @@ print(both)
 friends = ["Rolf", "Bob"]
 abroad = ["Rolf", "Bob"]
 
-# checks if both things are the same not just having same elements
+# checks if both lists are the same not just having same elements
 print(friends is abroad)
 print(friends is friends)
 
@@ -55,5 +55,41 @@ print(friends is friends)
 
 # in keyword (already understand this section)
 
-# If statements with the "in" keyword 
+# If statements with the "in" keyword
 
+# loops (already understand this section)
+
+# list comprehensions
+numbers = [1, 3, 5]
+doubled = [number * 2 for number in numbers]
+
+print(doubled)
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+starts_s = [name for name in friends if name.startswith("S")]
+
+print(starts_s)
+
+# Dictionaries (already understand this section)
+
+# Destructuring variables
+x, y = 5, 11
+
+print(x, y)
+
+# how to destruct a tuple
+people = [("Bob", 42, "Mechanic"), ("James", 24, "Artist")]
+
+for name, age, profession in people:
+    print(f"{name} is {age} and works as a {profession}")
+
+# if you want to ignore a part of the tuple
+person = ("Bob", 42, "Mechanic")
+name, _, profession = person
+print(f"{name} is a {profession}")
+
+# separate list
+head, *tail = [1, 2, 3, 4, 5]
+
+print(head)
+print(tail)
