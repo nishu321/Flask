b13ecd172179e99659c0cfb320713eccdecaaf3c
nishu321 <nissarg37@gmail.com>
2021-03-09 10:10:31 -0500

    Finished refresher on section 2 part 9-16

--- PythonRefresher/python_refresher.py
+++ PythonRefresher/python_refresher.py
@@ -11,5 +11,49 @@ print(greeting)
 x = 1000
 
 print(f"{x:,}")
+
 # get input (already understand this section)
+
+# write python first app
+years = int(input("Enter your age:"))
+months = years * 12
+
+print(f"Your age, {years}, is equal to {months} months.")
+
+# list, tuples, and sets (already understand this section)
+
+# Advanced set operations
+friends = {"Bob", "Rolf", "Anne"}
+abroad = {"Bob", "Anne"}
+
+local_friends = friends.difference(abroad)
+print(local_friends)
+
+local = {"Rolf"}
+abroad = {"Bob", "Anne"}
+
+friends = local.union(abroad)
+
+print(friends)
+
+art = {"Bob", "Jen", "Rolf", "Charlie"}
+science = {"Bob", "Jen", "Adam", "Anne"}
+
+both = art.intersection(science)
+
+print(both)
+
+# Boolean in Python
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+# checks if both things are the same not just having same elements
+print(friends is abroad)
+print(friends is friends)
+
+# If statements (already understand this section)
+
+# in keyword (already understand this section)
+
+# If statements with the "in" keyword 
 
