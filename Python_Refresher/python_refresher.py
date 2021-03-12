33fa95b52041e9f95fe097351e6d68b2c9f892b1
nishu321 <nissarg37@gmail.com>
2021-03-12 10:35:33 -0500

    Finished resfresher section 2 part 32

--- Python_Refresher/python_refresher.py
+++ Python_Refresher/python_refresher.py
@@ -284,3 +284,41 @@ class Book:
 
 book = Book.hard_cover("Harry Potter", 1500)
 print(book)
+
+
+# Class inheritance
+class Device:
+    def __init__(self, name, connected_by):
+        self.name = name
+        self.connected_by = connected_by
+        self.connected = True
+
+    def __str__(self):
+        return f"Device {self.name} {self.connected_by}"
+
+    def disconnect(self):
+        self.connected = False
+        print("Disconnected")
+
+
+class Printer(Device):
+    def __init__(self, name, connected_by, capcity):
+        super().__init__(name, connected_by)
+        self.capcity = capcity
+        self.remaining_pages = capcity
+
+    def __str__(self):
+        return f"{super().__str__()} {self.remaining_pages} pages remaining"
+
+    def print(self, pages):
+        if not self.connected:
+            print("Your printer is not connected")
+            return
+        print(f"Printing {pages} pages.")
+        self.remaining_pages -= pages
+
+
+printer = Printer("Printer", "USB", 500)
+printer.print(20)
+
+print(printer)
