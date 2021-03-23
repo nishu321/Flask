5ce5e41c921fde800f085156a7a3204f9719db13
nishu321 <nissarg37@gmail.com>
2021-03-22 20:19:04 -0400

    Finished refresher section 2 part 39

--- Python_Refresher/python_refresher.py
+++ Python_Refresher/python_refresher.py
@@ -463,3 +463,43 @@ try:
     book1.read(50)
 except TooManyPagesReadError as e:
     print(e)
+
+
+# first class functions
+def divide(dividened, divisor):
+    if divisor == 0:
+        raise ZeroDivisionError("Divisor cannot be 0.")
+
+    return dividened / divisor
+
+
+def calculate(*values, operator):
+    return operator(*values)
+
+
+result = calculate(20, 4, operator=divide)
+
+print(result)
+
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}.")
+
+
+friends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27}
+]
+
+def get_friend_name(friend):
+    return friend["name"]
+
+
+try:
+    print(search(friends, "Bob Smith", get_friend_name))
+except RuntimeError as e:
+    print(e)
