93854b2498bca76693667b2a68ef89f7f14bc2dc
nishu321 <nissarg37@gmail.com>
2021-05-27 18:51:54 -0400

    Created home and about page

--- First_Flask/first_flask.py
+++ First_Flask/first_flask.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
+info = {
+    "name": "Nishu Mehta",
+    "age": "27",
+    "job": "Software-developer"
+}
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template("index.html",title="Home",greeting=True)
+
+@app.route("/about")
+def about():
+    return render_template("about.html",title="About",info=info)
 
 if __name__ == "__main__":
     app.run(debug=True)
