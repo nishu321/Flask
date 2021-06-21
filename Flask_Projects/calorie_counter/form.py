7e61050ca679654a234a66cd236bdc5ccc9a8557
nishu321 <nissarg37@gmail.com>
2021-06-21 18:07:51 -0400

    Created calorie counter form

--- Flask_Projects/calorie_counter/form.py
+++ Flask_Projects/calorie_counter/form.py
@@ -1,4 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField,RadioField,IntegerField
+from wtforms.validators import DataRequired, Length, NumberRange
+
+class Counter(FlaskForm):
+    name = StringField(
+        "Name",
+        validators=[DataRequired()]
+    )
+
+    age = IntegerField(
+        "Age",
+        validators=[DataRequired(),Length(min=2, max=3,message="Please enter validate age.")]
+    )
+
+    weight = IntegerField(
+        "Weight",
+        validators=[DataRequired()]
+    )
+
+    sex = RadioField(
+        "Sex",
+        choices=[("Male","Male"),("Female","Female")]
+    )
+
+    goal = RadioField(
+        "Goal",
+        choices=[("Lose","Lose"),("Maintaine","Maintaine"),("Gain","Gain")]
+    )
+
+    fat = IntegerField(
+        "Fat",
+        validators=[DataRequired(),NumberRange(min=20,max=35,message="Please enter a number between 20 - 35")]
+    )
+
+    protein = IntegerField(
+        "Protein",
+        validators=[DataRequired(),NumberRange(min=10,max=35,message="Please enter a number between 10 - 35")]
+    )
+
+    carbs = IntegerField(
+        "Carbs",
+        validators=[DataRequired(),NumberRange(min=45,max=65,message="Please enter a number between 45- 65")]
+    )
 
