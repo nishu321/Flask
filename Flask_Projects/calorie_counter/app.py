6a9a4ec42419b0fac3f8b33b1d7b2734304a0ffa
nishu321 <nissarg37@gmail.com>
2021-06-22 12:57:06 -0400

    Created helper file

--- Flask_Projects/calorie_counter/app.py
+++ Flask_Projects/calorie_counter/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for
+from helper import users 
+from form import Counter
 
 app = Flask(__name__)
 app.config["SERCET_KEY"] = "17c03ca41147650a9458dd14c8613265"
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",users=users)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ Flask_Projects/calorie_counter/helper.py
@@ -0,0 +1,12 @@
+users = [
+    {
+        "name":"Nick",
+        "age":27,
+        "sex":"Male",
+        "goal":"Lose",
+        "calories":1625,
+        "fat":36,
+        "protien":142,
+        "carbs":182
+    }
+]
