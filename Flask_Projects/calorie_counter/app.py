3c292414bb1953d4a8b0f26a12c93e54c34ae2d4
nishu321 <nissarg37@gmail.com>
2021-06-24 12:26:44 -0400

    Created a list of user on Index page

--- Flask_Projects/calorie_counter/app.py
+++ Flask_Projects/calorie_counter/app.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template, url_for
 from helper import users 
 from form import Counter
 
+
 app = Flask(__name__)
 app.config["SERCET_KEY"] = "17c03ca41147650a9458dd14c8613265"
 
 @app.route("/")
 def index():
     return render_template("index.html",users=users)
+
+@app.route("/user_info/<int:id>")
+def user_info(id):
+    return "User info"
 
 if __name__ == "__main__":
     app.run(debug=True)
