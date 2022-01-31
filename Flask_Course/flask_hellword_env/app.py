2edf5497345e9ca7001cb26682be50ce838cc8d4
nishu321 <nissarg37@gmail.com>
2022-01-31 14:01:55 -0500

    Created Flask Hello World app

---
+++ Flask_Course/flask_hellword_env/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/fancy")
+def fancy_hello():
+    return """
+    <html>
+    <body>
+    <h1>Greetings!</h1>
+    <p>Hello, world</p>
+    </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run()
