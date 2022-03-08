008ba6ac17bbf7757213b98fc899341db3c1ca85
nishu321 <nissarg37@gmail.com>
2022-03-08 13:10:50 -0500

    Retreiving data from MongDB

--- Flask_Course/flask_microblog_env/app.py
+++ Flask_Course/flask_microblog_env/app.py
@@ -7,23 +7,20 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://Nishu:Nishu321@microblogapp.qgmqc.mongodb.net/test")
 app.db = client.microblog
 
-entries = []
-
 @app.route("/", methods=["GET","POST"])
 def home():
     if request.method == "POST":
         entery_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entery_content,formatted_date))
         app.db.entries.insert_one({"content": entery_content, "date": formatted_date})
 
     entries_with_date = [
         (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1],"%Y-%m-%d").strftime("%b %d")
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"],"%Y-%m-%d").strftime("%b %d")
         )
-        for entry in entries
+        for entry in app.db.entries.find()
     ]
     
     return render_template("home.html", entries=entries_with_date)
